5edaf0af48fe2f5b8efb3128c35e1027a5ef01a5
Katie Fleming <kfleming@olin.edu>
2021-09-23 17:31:45 -0400

    Revised sweep, new tilt pattern

--- Mini Project 2/src/main.cpp
+++ Mini Project 2/src/main.cpp
@@ -12,28 +12,21 @@ bool up = true;
 void setup() {
   myservoX.attach(9);  // attaches the servo on pin 9 to the servo object
   myservoY.attach(10);  // attaches the servo on pin 10 to the servo object
-                       // CHECK THAT THIS IS THE CORRECT PIN NUMBER!!!
+
+Serial.begin(9600);
 }
 
 void loop() {
     for (posX = 0; posX <= 180; posX += 5) {  // goes from 0 degrees to 180 degrees X axis
         myservoX.write(posX);                 // tell servo to go to position in variable 'posX'
         delay(15);                            // waits 15ms for the servo to reach the position
-       
-        if(up == true){  // decides if servo needs to rotate up in the Y axis
-            for (posY = 0; posY <= 180; posY += 5) {  // goes from 0 degrees to 180 degrees Y axis
-                myservoY.write(posY);                 // tell servo to go to position in variable 'posY'
-                delay(15);                            // waits 15ms for the servo to reach the position
-            }
-            up = !up;   //update up to false
+        
+        for (posY = 0; posY <= 180; posY += 5) {  // goes from 0 degrees to 180 degrees Y axis
+            myservoY.write(posY);                 // tell servo to go to position in variable 'posY'
+            delay(15);                            // waits 15ms for the servo to reach the position
         }
-        if(up == false){ // decides if servo needs to rotate down in the Y axis
-            for (posY = 180; posY >= 0; posY -= 5) {  // goes from 180 degrees to 0 degrees
-                myservoY.write(posY);                 // tell servo to go to position in variable 'posY'
-                delay(15);                            // waits 15ms for the servo to reach the position
-            }
-            up = !up;   // update up to true
-        }
+        myservoY.write(0);
+
     }
     myservoX.write(0);
 }
