f18f89b3354dab47bca264810ae7956429f7dd27
Katie Fleming <kfleming@olin.edu>
2021-09-25 15:12:58 -0400

    Sweep function now intakes sensor value and records to serial

--- Mini Project 2/src/main.cpp
+++ Mini Project 2/src/main.cpp
@@ -4,8 +4,12 @@
 Servo myservoX;  // create servo object to control a servo's X rotation
 Servo myservoY;  // create servo object to control a servo's Y rotation
 
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+
 int posX = 0;    // variable to store the servo position along X axis
 int posY = 0;    // variable to store the servo position along Y axis
+
+int sensorValue = 0;        // value read from the pot
 
 bool up = true;
 
@@ -19,11 +23,18 @@ Serial.begin(9600);
 void loop() {
     for (posX = 0; posX <= 180; posX += 5) {  // goes from 0 degrees to 180 degrees X axis
         myservoX.write(posX);                 // tell servo to go to position in variable 'posX'
-        delay(15);                            // waits 15ms for the servo to reach the position
+        delay(150);                            // waits 15ms for the servo to reach the position
+        sensorValue = analogRead(analogInPin);
+        Serial.print(posX); Serial.print(", ");
+        Serial.println(sensorValue);
         
         for (posY = 0; posY <= 180; posY += 5) {  // goes from 0 degrees to 180 degrees Y axis
             myservoY.write(posY);                 // tell servo to go to position in variable 'posY'
-            delay(15);                            // waits 15ms for the servo to reach the position
+            delay(150);                            // waits 15ms for the servo to reach the position
+            
+            sensorValue = analogRead(analogInPin);
+            Serial.print(posY); Serial.print(", ");
+            Serial.println(sensorValue);
         }
         myservoY.write(0);
 
