e419801d8be2265efabb591b7bb919755a7c87c8
lila <lsmith2@olin.edu>
2021-09-27 12:32:30 -0400

    Added comments and edited for clarity.

--- Mini Project 2/src/main.cpp
+++ Mini Project 2/src/main.cpp
@@ -5,37 +5,36 @@ Servo myservoX;  // create servo object to control a servo's X rotation
 Servo myservoY;  // create servo object to control a servo's Y rotation
 
 const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+const int servoXPin = 9; // Sets pin num for pan servo
+const int servoYPin = 10; // Sets pin num for tilt servo
+
+const int posLimitsX[] = {0, 180, 5}; // pan servo: starting angle, ending angle, angle increment
+const int posLimitsY[] = {0, 180, 5}; // tilt servo: starting angle, ending angle, angle increment
+
 
 int posX = 0;    // variable to store the servo position along X axis
 int posY = 0;    // variable to store the servo position along Y axis
 
-int sensorValue = 0;        // value read from the pot
-
-bool up = true;
+int sensorValue = 0;    // value read from the pot
 
 void setup() {
-  myservoX.attach(9);  // attaches the servo on pin 9 to the servo object
-  myservoY.attach(10);  // attaches the servo on pin 10 to the servo object
-
-Serial.begin(9600);
+  myservoX.attach(servoXPin);  
+  myservoY.attach(servoYPin); 
+  Serial.begin(9600);
 }
 
 void loop() {
-    for (posX = 0; posX <= 180; posX += 5) {  // goes from 0 degrees to 180 degrees X axis
+    for (posX = posLimitsX[0]; posX <= posLimitsX[1]; posX += posLimitsX[2]) {  // goes from 0 degrees to 180 degrees X axis
         myservoX.write(posX);                 // tell servo to go to position in variable 'posX'
-        delay(150);                            // waits 15ms for the servo to reach the position
+        delay(150);                           // waits 15ms for the servo to reach the position
         sensorValue = analogRead(analogInPin);
         
-        for (posY = 0; posY <= 180; posY += 5) {  // goes from 0 degrees to 180 degrees Y axis
+        for (posY = posLimitsY[0]; posY <= posLimitsY[1]; posY += posLimitsY[2]) {  // goes from 0 degrees to 180 degrees Y axis
             myservoY.write(posY);                 // tell servo to go to position in variable 'posY'
             delay(150);                            // waits 15ms for the servo to reach the position
-            
-            sensorValue = analogRead(analogInPin);
-            Serial.print(posX); Serial.print(", "); Serial.print(posY); Serial.print(", ");
-            Serial.println(sensorValue);
-        }
-        myservoY.write(0);
 
+            sensorValue = analogRead(analogInPin); 
+            Serial.println((String) posX + ", " + posY + ", " + sensorValue); // prints data to be recorded with python script
+        }
     }
-    myservoX.write(0);
 }
