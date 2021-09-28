9a1afaf0fb63c180ce71b1b5341c3dcb298b064e
lila <lsmith2@olin.edu>
2021-09-28 18:18:48 -0400

    Added cartesian conversion and refactored.

--- Mini Project 2/src/main.cpp
+++ Mini Project 2/src/main.cpp
@@ -8,14 +8,15 @@ const int analogInPin = A0;  // Analog input pin that the potentiometer is attac
 const int servoXPin = 9; // Sets pin num for pan servo
 const int servoYPin = 10; // Sets pin num for tilt servo
 
-const int posLimitsX[] = {0, 180, 5}; // pan servo: starting angle, ending angle, angle increment
-const int posLimitsY[] = {0, 180, 5}; // tilt servo: starting angle, ending angle, angle increment
+const int posLimitsX[] = {0, 50, 10}; // pan servo: starting angle, ending angle, angle increment
+const int posLimitsY[] = {0, 50, 10}; // tilt servo: starting angle, ending angle, angle increment
 
 
 int posX = 0;    // variable to store the servo position along X axis
 int posY = 0;    // variable to store the servo position along Y axis
 
 int sensorValue = 0;    // value read from the pot
+int outputValue = 0;    // value mapped to 256
 
 void setup() {
   myservoX.attach(servoXPin);  
@@ -24,6 +25,9 @@ void setup() {
 }
 
 void loop() {
+    delay(3000);
+    Serial.println("Begin");
+    delay(2000);
     for (posX = posLimitsX[0]; posX <= posLimitsX[1]; posX += posLimitsX[2]) {  // goes from 0 degrees to 180 degrees X axis
         myservoX.write(posX);                 // tell servo to go to position in variable 'posX'
         delay(150);                           // waits 15ms for the servo to reach the position
@@ -34,7 +38,9 @@ void loop() {
             delay(150);                            // waits 15ms for the servo to reach the position
 
             sensorValue = analogRead(analogInPin); 
-            Serial.println((String) posX + ", " + posY + ", " + sensorValue); // prints data to be recorded with python script
+            outputValue = map(sensorValue, 0, 1023, 0, 255);
+            Serial.println((String) posX + ", " + posY + ", " + outputValue); // prints data to be recorded with python script
         }
     }
+    Serial.println("End");
 }
--- PythonReceiveData.py
+++ PythonReceiveData.py
@@ -1,28 +1,62 @@
-import serial
+import serial, time, math
+import numpy as np
+
+def cosd(num):
+  return math.cos(math.radians(num))
+
+
+def sind(num):
+  return math.sin(math.radians(num))
+
+def output2cart(input):
+  pan, tilt, output = input
+  a = 1736 
+  b = -0.8824
+  distance = (output/a)**(1/b)
+  x = distance * cosd(pan) * sind(tilt)
+  y = distance * sind(pan) * sind(tilt)
+  z = distance * cosd(tilt)
+  return x,y,z
+
 
 #name of serial port
-arduinoComPort = "COM4"
+arduinoComPort = "/dev/ttyACM0"
 
 #baudrate
 baudRate = 9600
 
 # set up the serial line
-ser = serial.Serial('COM4', 9600)
+serialPort = serial.Serial(arduinoComPort, 9600)
 time.sleep(2)
 
 #Send Arduino a Signal
-
+pan_angles = (180 - 0)/5
+tilt_angles = (180 - 0)/5
 
 # Read and record the data
-data =[]                       # empty list to store the data
-for i in range(100):
-  line = serialPort.readline().decode()  # convert the byte string to a unicode string
-  if len(line) > 0:
-    a, b, c = (int(x) for x in line.split(','))  #converts the strings into integer
-    print(a,b,c)
-    data.append(a,b,c)     # add int to data lists
+data = np.array([[0,0,0]])                    # empty list to store the data
 
-ser.close()
+
+line = ""               # stores serial output
+while not serialPort.readline().decode().strip() == "Begin":
+  print("waiting to begin")
+
+while not serialPort.readline().decode().strip() == "End":
+  line = serialPort.readline().decode() # convert the byte string to a unicode string
+  while len(line.split(",")) < 3:
+    line = serialPort.readline().decode() # convert the byte string to a unicode string
+  pan_angle, tilt_angle, sensor_output = (int(x) for x in line.strip().split(","))  #converts the strings into integer
+  print(pan_angle,tilt_angle,sensor_output)
+  data = np.append(data, [[pan_angle,tilt_angle,sensor_output]], axis=0)
+
+print("end")
+serialPort.close()
+
+data = np.delete(data, 0, 0)
+
+data_cart = np.array([output2cart(datapoint) for datapoint in data])
+
+print(data_cart)
 
 import matplotlib.pyplot as plt
 
@@ -31,13 +65,3 @@ plt.xlabel('Angles')
 plt.ylabel('Servo Reading')
 plt.title('Servo Reading vs. Angle')
 plt.show()
-
-
-
-
-
-
-
-
-
-
