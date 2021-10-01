419b29ad313e3ee4de8258e2268a3ca413524954
lila <lsmith2@olin.edu>
2021-09-30 20:46:56 -0400

    Added comments and edited for clarity.

--- Mini Project 2/src/main.cpp
+++ Mini Project 2/src/main.cpp
@@ -9,8 +9,8 @@ const uint8_t analogInPin = A0;  // Analog input pin that the potentiometer is a
 const uint8_t servoXPin = 9; // Sets pin num for pan servo
 const uint8_t servoYPin = 10; // Sets pin num for tilt servo
 
-const uint8_t posLimitsX[] = {0, 45, 1}; // pan servo: starting angle, ending angle, angle increment
-const uint8_t posLimitsY[] = {80, 120, 1}; // tilt servo: starting angle, ending angle, angle increment
+const uint8_t posLimitsX[] = {0, 35, 1}; // pan servo: starting angle, ending angle, angle increment
+const uint8_t posLimitsY[] = {75, 130, 1}; // tilt servo: starting angle, ending angle, angle increment
 
 
 uint8_t posX = 0;    // variable to store the servo position along X axis
@@ -19,14 +19,13 @@ uint8_t posY = 0;    // variable to store the servo position along Y axis
 int sensorValue = 0;    // value read from the pot
 uint8_t outputValue = 0;    // value mapped to 255
 
-const uint8_t numReadings = 100; // number of times to take a reading at each point
+const uint8_t numReadings = 5; // number of times to take a reading at each point
 uint8_t tempReadings[numReadings]; // create array to store readings
 
 int cmp_func(const void *num1, const void *num2) // used by qsort to sort the array
 {
   return *(int*) num1 - *(int*) num2;
 }
-
 
 void setup() {
   myservoX.attach(servoXPin);  
@@ -40,36 +39,45 @@ void loop() {
     delay(2000);
     for (posX = posLimitsX[0]; posX <= posLimitsX[1]; posX += posLimitsX[2]) { 
         myservoX.write(posX);                 // tell servo to go to position in variable 'posX'
-        
-        for (posY = posLimitsY[0]; posY <= posLimitsY[1]; posY += posLimitsY[2]) {  // goes from 0 degrees to 180 degrees Y axis
+        for (posY = posLimitsY[0]; posY <= posLimitsY[1]; posY += posLimitsY[2]) { 
             myservoY.write(posY);                 // tell servo to go to position in variable 'posY'
-            delay(15);                            // waits 15ms for the servo to reach the position
-            for (int i = 0; i < numReadings; i++) {
+            delay(150);                            // waits 150ms for the servo to reach the position
+            for (int i = 0; i < numReadings; i++) { // record numReadings datapoints
                 tempReadings[i] = analogRead(analogInPin);
             }
             qsort(tempReadings, numReadings, sizeof(tempReadings[0]), cmp_func); // sort the array into ascending order
             sensorValue = tempReadings[(int) numReadings/2 - 1]; // take the median of values
-            outputValue = map(sensorValue, 0, 1023, 0, 255);
-            Serial.println((String) posX + ", " + posY + ", " + outputValue); // prints data to be recorded with python script
-        }
+            outputValue = map(sensorValue, 0, 1023, 0, 255); // map to 0-255
+            if (outputValue > 30 && outputValue < 110) { // only send value if inside of bounds of IR sensor specs
+                Serial.println((String) posX + ", " + posY + ", " + outputValue); // Serial to be recorded with python script   
+            }
+    
     }
     Serial.println("End");
 }
 
-/* 
-// One servo version
+
+/* // One servo version
 Servo myservoY;  // create servo object to control a servo's Y rotation
 
-const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
-const int servoYPin = 10;    // Sets pin num for tilt servo
+const uint8_t analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+const uint8_t servoYPin = 10; // Sets pin num for tilt servo
 
-const int posLimitsY[] = {15, 165, 10}; // tilt servo: starting angle, ending angle, angle increment
+const uint8_t posLimitsY[] = {80, 140, 1}; // tilt servo: starting angle, ending angle, angle increment
 
 
-int posY = 0;                // variable to store the servo position along Y axis
+uint8_t posY = 0;    // variable to store the servo position along Y axis
 
-int sensorValue = 0;         // value read from the pot
-int outputValue = 0;         // value mapped to 256
+int sensorValue = 0;    // value read from the pot
+uint8_t outputValue = 0;    // value mapped to 255
+
+const uint8_t numReadings = 100; // number of times to take a reading at each point
+uint8_t tempReadings[numReadings]; // create array to store readings
+
+int cmp_func(const void *num1, const void *num2) // used by qsort to sort the array
+{
+  return *(int*) num1 - *(int*) num2;
+}
 
 void setup() {
   myservoY.attach(servoYPin); 
@@ -79,12 +87,18 @@ void setup() {
 void loop() {
     delay(3000);
     Serial.println("Begin");
-    for (posY = posLimitsY[0]; posY <= posLimitsY[1]; posY += posLimitsY[2]) {  // goes from 0 degrees to 180 degrees Y axis
+    for (posY = posLimitsY[0]; posY <= posLimitsY[1]; posY += posLimitsY[2]) { 
         myservoY.write(posY);                 // tell servo to go to position in variable 'posY'
-        delay(150);                            // waits 15ms for the servo to reach the position
-        sensorValue = analogRead(analogInPin);
-        outputValue = map(sensorValue, 0, 1023, 0, 255);
-        Serial.println((String) 0 + ", " + posY + ", " + outputValue); // prints data to be recorded with python script
+        delay(150);                            // waits 150ms for the servo to reach the position
+        for (int i = 0; i < numReadings; i++) { // record numReadings datapoints
+            tempReadings[i] = analogRead(analogInPin);
+        }
+        qsort(tempReadings, numReadings, sizeof(tempReadings[0]), cmp_func); // sort the array into ascending order
+        sensorValue = tempReadings[(int) numReadings/2 - 1]; // take the median of values
+        outputValue = map(sensorValue, 0, 1023, 0, 255); // map to 0-255
+        if (outputValue > 30 && outputValue < 110) { // only send value if inside of bounds of IR sensor specs
+            Serial.println((String) posX + ", " + posY + ", " + outputValue); // Serial to be recorded with python script   
+        }
     }
     Serial.println("End");
 } */
--- PythonReceiveData.py
+++ PythonReceiveData.py
@@ -3,31 +3,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def output2cart(input):
+  """
+  The purpose of this function is to convert angles and sensor output to
+  cartesian coordinates.
+
+  input: n by 3 array in order pan, tilt, and output
+  
+  Returns x,y,z in cm.
+  """
   pan, tilt, output = input.T
+  # These values were found with MATLAB's cf tool to translate output to actual
+  # distance. They fit the exponential below.
   a = 1736 
   b = -0.8824
   distance = (output/a)**(1/b)
-  x = distance * np.sin(np.radians(pan)) * np.cos(np.radians(90-tilt))
-  y = distance * np.sin(np.radians(pan)) * np.sin(np.radians(90-tilt))
-  z = distance * np.cos(np.radians(90-tilt))
+  # Spherical polar coordinates to cartesian coordinates
+  x = distance * np.cos(np.radians(pan)) * np.sin(np.radians(180 - tilt))
+  y = distance * np.sin(np.radians(pan)) * np.sin(np.radians(180 - tilt))
+  z = distance * np.cos(np.radians(180 - tilt))
   return x,y,z
 
 def plot(input):
+  """
+  Plots 3D scatter of points.
+
+  input: 3 by n array in order x, y, and z
+  """
   # Creating figure
   fig = plt.figure(figsize = (10, 7))
   ax = plt.axes(projection ="3d")
   
   # Creating plot
-  ax.scatter3D(input[0], input[1], input[2], color = "green")
+  ax.scatter3D(input[0], input[1], input[2])
   plt.title("simple 3D scatter plot")
   
-  ax.set_xlabel('X axis')
-  ax.set_ylabel('Y axis')
-  ax.set_zlabel('Z axis')
+  ax.set_xlabel('X axis (cm)')
+  ax.set_ylabel('Y axis (cm)')
+  ax.set_zlabel('Z axis (cm)')
 
   # show plot
   plt.show()
-
 
 # ====== Setup Values ======
 #name of serial port
@@ -45,16 +60,20 @@ time.sleep(2)
 while not serialPort.readline().decode().strip() == "Begin":
   print("Waiting To Begin")
 
-  # Continues to collect data until "End"
+# Continues to collect data until "End"
 while not line == "End":
-  # Checks that line follows three number format
+  # Checks that line follows three number format before recording
   if len(line.split(",")) > 2:
     pan_angle, tilt_angle, sensor_output = (int(x) for x in line.strip().split(","))  #converts the strings into integer
     print(pan_angle,tilt_angle,sensor_output)
     data = np.append(data, [[pan_angle,tilt_angle,sensor_output]], axis=0)
-  line = serialPort.readline().decode().strip() # Get next line 
+  line = serialPort.readline().decode().strip() # Get next line
+
+# Remove row used to initialize data
+data = np.delete(data,0,0)
 
 print("End")
 serialPort.close()
 
+# Use functions above to visualize data
 plot(output2cart(data))
